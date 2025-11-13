92b4be911b8855adb99775092dbdc8e8a5687fe9
Alexandre Angrignon <alex040892@gmail.com>
2025-11-13 19:23:50 +1300

    BrowserRouter Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import { Link, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Agence from './pages/Agence';
+import Projects from './pages/Projects';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className='text-6xl text-white'>App</div>
+      <div className=' text-white'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/agence' element={<Agence />} />
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+      </div>
     </>
   );
 }
