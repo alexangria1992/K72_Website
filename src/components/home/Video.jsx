48bd55bd61f055f2b6c542cc053a12ac1a742d58
Alexandre Angrignon <alex040892@gmail.com>
2025-11-13 19:45:55 +1300

    Video Background

---
+++ src/components/home/Video.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Video = () => {
+  return (
+    <div className='h-full w-full'>
+      <video
+        className='w-full h-full object-cover'
+        autoPlay
+        loop
+        muted
+        src='../../../public/video.mp4'
+      ></video>
+    </div>
+  );
+};
+
+export default Video;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Video from '../components/home/Video';
+import HomeHeroText from '../components/home/HomeHeroText';
+import HomeBottomText from '../components/home/HomeBottomText';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className='h-screen w-screen fixed'>
+        <Video />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
