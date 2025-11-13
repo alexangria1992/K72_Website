fdd0da1baece4679d92135da1652c842f5f0cd43
Alexandre Angrignon <alex040892@gmail.com>
2025-11-13 22:14:47 +1300

    Home Bottom Text + Links

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomeBottomText = () => {
-  return <div>HomeBottomText</div>;
+  return (
+    <div className='font-[font2] flex items-center justify-center gap-2'>
+      <div className='hover:border-[#D3FD50] hover:text-[#D3FD50]  border-3 h-44 flex items-center  border-white rounded-full px-14  uppercase'>
+        <Link className='text-[6.5vw] mt-6' to='/projects'>
+          Projects
+        </Link>
+      </div>
+      <div className='hover:border-[#D3FD50] hover:text-[#D3FD50]  border-3 h-44 flex items-center  border-white rounded-full px-14  uppercase'>
+        <Link className='text-[6.5vw] mt-6' to='/agence'>
+          Agence
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default HomeBottomText;
